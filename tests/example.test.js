6f61467ea0c3365e4a5f4f9d58952d3ca748fc27
Jukka-Pekka Keisala <jpkeisala@outlook.com>
2021-08-18 08:57:22 +0200

    second commit

---
+++ tests/example.test.js
@@ -0,0 +1,17 @@
+const puppeteer = require('puppeteer')
+
+describe('My First Puppeteer Test', ()  => {
+    it('should launch the browser', async function() {    
+        const browser = await puppeteer.launch({
+            headless: false, 
+            slowMo: 10, 
+            devtools:false,
+        })
+        const page = await browser.newPage()
+        await page.goto('http://example.com/')
+        await page.wa
+        await page.waitForTimeout(1000)
+        await page.waitForSelector('h1')
+        await browser.close()
+    })
+})
