cdbd88c23018f013b6c037a3932e097d6ada33ad
Jukka-Pekka Keisala <jpkeisala@outlook.com>
2021-08-18 09:24:54 +0200

    example test

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,17 +1,35 @@
 const puppeteer = require('puppeteer')
 
 describe('My First Puppeteer Test', ()  => {
-    it('should launch the browser', async function() {    
-        const browser = await puppeteer.launch({
+    let browser
+    let page
+
+    before(async function(){
+        browser = await puppeteer.launch({
             headless: false, 
             slowMo: 10, 
             devtools:false,
-        })
-        const page = await browser.newPage()
+        })  
+        page = await browser.newPage()
+        await page.setDefaultTimeout(10000)
+        await page.setDefaultNavigationTimeout(20000)
+    })
+
+    after(async function(){
+        await browser.close()
+    })
+
+
+
+    it('should launch the browser', async function() {    
         await page.goto('http://example.com/')
-        await page.wa
         await page.waitForTimeout(1000)
         await page.waitForSelector('h1')
-        await browser.close()
+        await page.goto('http://zero.webappsecurity.com/')
+        await page.waitForSelector('#signin_button')
+        await page.click('#signin_button')
+        //await page.waitForResponse(() => !document.querySelector('#signin_button'))
+        await page.waitForSelector('#signin_button', {hidden: true, timeout:3000,})
+        
     })
 })
